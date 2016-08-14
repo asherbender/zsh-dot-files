e830464be319af8f42eeb05f0d9bc3e630cc7a06
Asher Bender <a.bender.dev@gmail.com>
2016-08-14 18:20:38 +1000

    autosuggestion: Updated loading for newer package

--- plugins/autosuggestions.zsh
+++ plugins/autosuggestions.zsh
@@ -13,37 +13,11 @@
 #      than line-oriented behaviour).
 #
 if [ -d $AUTOSUGGESTIONS ]; then
-    source "${AUTOSUGGESTIONS}/autosuggestions.zsh"
-
-    # Enable autosuggestions automatically.
-    zle-line-init() {
-        zle autosuggest-start
-    }
-    zle -N zle-line-init
+    source "${AUTOSUGGESTIONS}/zsh-autosuggestions.zsh"
 
     # Customise autosuggest syntax highlighting.
-    AUTOSUGGESTION_HIGHLIGHT_CURSOR=1
-    AUTOSUGGESTION_HIGHLIGHT_COLOR='fg=black,underline'
+    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,underline'
 
     # Allow suggestions to be accepted mid-line.
-    bindkey '^J' autosuggest-execute-suggestion
-
-    # Allow autosuggest to be toggled on/off (make function verbose so the
-    # autosuggestion's state is known).
-    verbose-autosuggest-toggle() {
-	if [[ -n $ZLE_AUTOSUGGESTING ]]; then
-	    autosuggest-pause
-	    zle -A autosuggest-self-insert-orig self-insert
-            # [[ -o zle ]] && zle -I
-            # print "$fg[red]autosuggest OFF"
-            zle -M "autosuggest OFF"
-	else
-	    autosuggest-resume
-            # [[ -o zle ]] && zle -I
-            # print "$fg[green]autosuggest ON"
-            zle -M "autosuggest ON"
-	fi
-    }
-    zle -N verbose-autosuggest-toggle
-    bindkey '^Z' verbose-autosuggest-toggle
+    bindkey '^J' autosuggest-execute
 fi
